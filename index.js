d551f6acb96acbd4ab82acc6f828fbd31f4691d5
Sanskargh7 <sanskargh5222@gmail.com>
2022-11-21 17:54:59 +0530

    first commit

--- index.js
+++ index.js
@@ -89,3 +89,73 @@ window.addEventListener('load', () => {
     })
 
 })
+document.addEventListener('click',(e)=>{
+
+    if (e.target.className=='shop-item-button'){
+        const prodId = Number(e.target.parentNode.parentNode.id.split('-')[1]);
+        axios.post('http://localhost:3000/cart', { productId: prodId}).then(data => {
+            if(data.data.error){
+                throw new Error('Unable to add product');
+            }
+            showNotification(data.data.message, false);
+        })
+        .catch(err => {
+            console.log(err);
+            showNotification(err, true);
+        });
+
+    }
+    if (e.target.className=='cart-btn-bottom' || e.target.className=='cart-bottom' || e.target.className=='cart-holder'){
+        axios.get('http://localhost:3000/cart').then(carProducts => {
+            showProductsInCart(carProducts.data);
+            document.querySelector('#cart').style = "display:block;"
+
+        })
+    }
+    if (e.target.className=='cancel'){
+        document.querySelector('#cart').style = "display:none;"
+    }
+    if (e.target.className=='purchase-btn'){
+        if (parseInt(document.querySelector('.cart-number').innerText) === 0){
+            alert('You have Nothing in Cart , Add some products to purchase !');
+            return
+        }
+        alert('This Feature is yet to be completed ')
+    }
+})
+
+function showProductsInCart(listofproducts){
+    cart_items.innerHTML = "";
+    listofproducts.forEach(product => {
+        const id = `album-${product.id}`;
+        const name = document.querySelector(`#${id} h3`).innerText;
+        const img_src = document.querySelector(`#${id} img`).src;
+        const price = product.price;
+        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)+1
+        const cart_item = document.createElement('div');
+        cart_item.classList.add('cart-row');
+        cart_item.setAttribute('id',`in-cart-${id}`);
+        cart_item.innerHTML = `
+        <span class='cart-item cart-column'>
+        <img class='cart-img' src="${img_src}" alt="">
+            <span>${name}</span>
+        </span>
+        <span class='cart-price cart-column'>${price}</span>
+        <form onsubmit='deleteCartItem(event, ${product.id})' class='cart-quantity cart-column'>
+            <input type="text" value="1">
+            <button>REMOVE</button>
+        </form>`
+        cart_items.appendChild(cart_item)
+    })
+}
+function showNotification(message, iserror){
+    const container = document.getElementById('container');
+    const notification = document.createElement('div');
+    notification.style.backgroundColor = iserror ? 'red' : 'green';
+    notification.classList.add('notification');
+    notification.innerHTML = `<h4>${message}<h4>`;
+    container.appendChild(notification);
+    setTimeout(()=>{
+        notification.remove();
+    },2500)
+}
